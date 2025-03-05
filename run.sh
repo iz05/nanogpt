4302645cd87a7b6db83862f4ba953f3a2b3e6e8f
Isabella Zhu <ixzhu@mit.edu>
2025-03-05 15:35:51 -0500

    python3, not python

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 for n in 2 4 6 8 10; do
     export N_LAYER=$n
     echo "Starting training with n_layer=$n"
-    python train.py config/train_tinystories.py &
+    python3 train.py config/train_tinystories.py &
 done
 
 # Wait for all background processes to finish
